600d80cb5947b20c68c9f355fe201249175eb1a2
chenhan <chenhan@jixiang.cn>
2025-12-04 11:03:20 +0800

    1.0.0

---
+++ RXGDTSDK.framework/Headers/RXGDTSDK.h
@@ -0,0 +1,18 @@
+//
+//  RXGDTSDK.h
+//  RXGDTSDK
+//
+//  Created by 陈汉 on 2025/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RXGDTSDK.
+FOUNDATION_EXPORT double RXGDTSDKVersionNumber;
+
+//! Project version string for RXGDTSDK.
+FOUNDATION_EXPORT const unsigned char RXGDTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RXGDTSDK/PublicHeader.h>
+
+#import <RXGDTSDK/RXGDTService.h>
